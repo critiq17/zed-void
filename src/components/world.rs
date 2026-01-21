64a5c915ab70fe59bd50a9a4cc66d400736fd430
critiq17 <critiq17@gmail.com>
2026-01-21 18:24:55 +0100

    world.rs for world map with objects && hitboxes

---
+++ src/components/world.rs
@@ -0,0 +1,82 @@
+use bevy::prelude::*;
+
+
+#[derive(Component, Clone, Copy)]
+pub struct Tile {
+    pub grid_x: i32, 
+    pub grid_y: i32,      
+    pub tile_type: TileType,  
+}
+
+impl Tile {
+    pub fn new(grid_x: i32, grid_y: i32, tile_type: TileType) -> Self {
+        Self {
+            grid_x,
+            grid_y,
+            tile_type,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum TileType {
+    GrassLight,  
+    GrassDark,   
+    Dirt,       
+    Stone,       
+    Water,       
+}
+
+impl TileType {
+
+    pub fn color(&self) -> Color {
+        use crate::constants::colors;
+        match self {
+            TileType::GrassLight => colors::GRASS_LIGHT,
+            TileType::GrassDark => colors::GRASS_DARK,
+            TileType::Dirt => colors::DIRT,
+            TileType::Stone => colors::STONE,
+            TileType::Water => Color::srgb(0.2, 0.4, 0.8),
+        }
+    }
+    
+   
+    pub fn is_walkable(&self) -> bool {
+        match self {
+            TileType::Water => false, 
+            _ => true,
+        }
+    }
+}
+
+
+#[derive(Component)]
+pub struct Building {
+    pub building_type: BuildingType,
+    pub grid_x: i32,
+    pub grid_y: i32,
+    pub width: u32,   
+    pub height: u32,  
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum BuildingType {
+    House,
+    Shed,
+    Wall,
+}
+
+
+#[derive(Component)]
+pub struct Furniture {
+    pub furniture_type: FurnitureType,
+    pub is_blocking: bool,  
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum FurnitureType {
+    Table,
+    Chair,
+    Bed,
+    Container,  
+}
